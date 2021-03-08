f21728006b0852c6ade98c0f1282dd5c2dbf6ab5
Patipat Chewprecha <patipat.che@mahidol.edu>
2021-03-08 20:17:52 +0700

    add variable

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  serverId = 10;
+  serverStatus = 'offline';
   constructor() { }
 
   ngOnInit(): void {
+  }
+  getServerStatus(): string{
+    return this.serverStatus;
   }
 
 }
